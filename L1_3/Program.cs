ccf26c91bdb5f31dcffbf98afb831122bcab65cc
SolitE <solitte@yandex.ru>
2022-08-18 23:46:20 +0500

    Summa

---
+++ L1_3/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Vvedite chislo1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Vvedite chislo2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int sum = number1+number2;
+Console.WriteLine("Summa " + sum);
+
