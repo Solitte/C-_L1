5bca41587a3b0901770a33de8d13cc34302d4680
SolitE <solitte@yandex.ru>
2022-08-19 01:21:43 +0500

    add if

---
+++ L1_5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Vvedite vashe imya");
+string name = Console.ReadLine();
+
+if (name.ToLower() == "andrey")
+{
+    Console.WriteLine("Ya tebya L");
+}
+else
+{
+   Console.WriteLine("fu"); 
+}
