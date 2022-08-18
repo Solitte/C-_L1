ac7feba5b9a1cc78a7c88da442587527ab8ec237
SolitE <solitte@yandex.ru>
2022-08-19 00:38:00 +0500

    RandomX

---
+++ L1_4/Program.cs
@@ -0,0 +1,5 @@
+﻿int n1 = new Random().Next(1git45); // 11,12,13...44
+int n2 = new Random().Next(7,10); // 7,8,9
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine("sum " +(n1+n2));
