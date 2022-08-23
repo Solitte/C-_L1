0446fc32d212abb358859d0604fa62a94501cc33
SolitE <solitte@yandex.ru>
2022-08-24 03:45:38 +0500

    FindMaxDigit_999

---
+++ S2_1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int FindMaxDigit (int number)
+{
+    int ed = number%10;
+    int des = (number%100)/10;
+    int sot = number/100;
+    int max;
+
+    if (ed>des) 
+    {
+      max = ed;  
+    }
+    
+    else 
+    {
+      max = des;
+    }
+
+    if (max<sot)
+    {
+     max = sot;
+    }
+    return max;
+       
+}
+int rand = new Random().Next(100,1000);
+
+int maxDigit = FindMaxDigit(rand);
+Console.WriteLine($"maximal digit of {rand} is {maxDigit}");
