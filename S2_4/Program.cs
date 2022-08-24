964b741c6f784d7933d1510fa69afd86229ea714
SolitE <solitte@yandex.ru>
2022-08-25 02:51:03 +0500

    Proverka kvadratov

---
+++ S2_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Является ли первое число квадратом второго или наоборот
+bool Proverka(int number1, int number2)
+{
+if (number1*number1 == number2 || number2*number2 == number1)
+{
+    return true;
+}
+else
+{
+    return false;
+}
+}
+
+Console.WriteLine("Input integer number1 ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input integer number2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+bool result = Proverka(number1, number2);
+Console.WriteLine($"Proverka: {result}");
