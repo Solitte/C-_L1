355b1c7d9d1df7220b500992d7a586915df7f0ee
SolitE <solitte@yandex.ru>
2022-08-25 02:25:23 +0500

    Proverka

---
+++ S2_3/Program.cs
@@ -0,0 +1,20 @@
+﻿bool Proverka(int number, int a, int b)
+{
+if (number%a == 0 && number%b == 0)
+{
+    return true;
+}
+else
+{
+    return false;
+}
+}
+
+Console.WriteLine("Imput integer number ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Imput integer a ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Imput integer b ");
+int b = Convert.ToInt32(Console.ReadLine());
+bool result = Proverka(number, a, b);
+Console.WriteLine($"Proverka na kratnoct`: {result}");
