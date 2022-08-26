0797c533ea57296270f9f25d12f0d01a98ae0b2d
SolitE <solitte@yandex.ru>
2022-08-27 02:18:57 +0500

    S3_1

---
+++ S3_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Ввести номер четверти, в которой находится точка и получить результат диапозон (x,y)
+
+void Diapozon(int num)
+{
+    if (num == 1) Console.WriteLine("Diapozon x>0 i y>0");
+    if (num == 2) Console.WriteLine("Diapozon x<0 i y>0");    
+    if (num == 3) Console.WriteLine("Diapozon x<0 i y<0");    
+    if (num == 4) Console.WriteLine("Diapozon x>0 i y<0");
+    if (num <1 || num >4) Console.WriteLine("Neverno vvedena chetvert");
+
+}
+Console.WriteLine("Vedite nomer chetverti");
+int number = Convert.ToInt32(Console.ReadLine());
+Diapozon(number);
