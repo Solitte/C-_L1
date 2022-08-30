73f3e67314eaa2a390c27f5dd7d25477667adf26
SolitE <solitte@yandex.ru>
2022-08-31 00:36:59 +0500

    N!

---
+++ S4/1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти !N
+
+int Factorial(int N)
+{
+int sum = 1;
+for (int i = 2; i <= N; i++)
+{
+sum = sum * i;
+}
+return sum;
+}
+Console.WriteLine("Vedite polojitelnoe chislo");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Factorial chisla {number} is {Factorial(number)}");
